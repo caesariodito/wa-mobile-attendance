2a4500a02155bd66aa3ee8c20708df323492b485
NixOS - CD <caesariodito@gmail.com>
2024-09-09 15:51:33 +0700

    init: flake, direnv, gitignore, hello.py

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A very basic flake with a shell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: 
+  flake-utils.lib.eachDefaultSystem (
+    system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.default = pkgs.mkShell { 
+        packages = with pkgs; [
+          python3
+          virtualenv
+        ]; 
+
+        # buildInputs = with pkgs; [
+        #   chromium
+        # ];
+      };
+    }
+  );
+}
